16bdd5bcfcb14fbfa68947480c23f654237f7c58
Ben Kang <rkdgusdn9@gmail.com>
2020-04-03 05:24:43 +1300

    Navbar hovor colour changes

--- src/feature/navBar.tsx
+++ src/feature/navBar.tsx
@@ -5,7 +5,7 @@ export const NavBar: React.FC = () => {
     return (
       <nav className="nav-bar">
         <ul className="nav-bar__list">
-          <li><a href="#" className="nav-bar__logo" id="logo"><img src={logo} height="70" width="130" /></a></li>
+          <li><a href="/" className="nav-bar__logo" id="logo"><img src={logo} height="70" width="130" /></a></li>
             <div className="nav-bar__item-list">
               <li><a href="#" className="nav-bar__item" id="whyUs">WHY US?</a></li>
               <li><a href="#" className="nav-bar__item" id="memberships">MEMBERSHIPS</a></li>
