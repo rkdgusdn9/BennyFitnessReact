fc170fc434dbbfd355c8394c302172d4de04e310
Ben Kang <rkdgusdn9@gmail.com>
2020-04-03 23:42:18 +1300

    Router change from hash to browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import './assets/styles/index.scss';
-import { NavBar } from './feature/navBar';
 import { Footer } from './feature/footer';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { HomePage } from './pages/home';
 import { JobsPage } from './pages/jobs';
 import { TestPage } from './pages/test';
+import { NavBar } from './feature/navBar';
 
 function App() {
-  return (
-    <>
-      <NavBar/>
-        <BrowserRouter>
-            <Route path="/" component={HomePage} exact/>
-            <Route path="/jobs" component={JobsPage} />
-            <Route path="/test" component={TestPage} />
-        </BrowserRouter>
-      <Footer/>
-    </>
-  );
+	return (
+		<>
+			<NavBar />
+			<BrowserRouter>
+				<Route path="/" component={HomePage} exact />
+				<Route path="/jobs" component={JobsPage} />
+				<Route path="/test" component={TestPage} />
+			</BrowserRouter>
+			<Footer />
+		</>
+	);
 }
 
 export default App;
