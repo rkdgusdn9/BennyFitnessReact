342e40b1598599f13a2fe06f81db2f61a9fe8464
Ben Kang <rkdgusdn9@gmail.com>
2020-04-08 10:04:39 +1200

    Responsive UI (pixel to percentage)

--- src/feature/navBar.tsx
+++ src/feature/navBar.tsx
@@ -5,39 +5,45 @@ export const NavBar: React.FC = () => {
 	return (
 		<nav className="nav-bar">
 			<div className="nav-bar__container">
-				<ul className="list-items">
-					<li>
-						<a href="/" className="list-items__logo" id="logo">
-							<img src={logo} alt="BennyFitness" height="80" width="150" />
-						</a>
-					</li>
-
-					<li>
-						<a href="/whyUs" className="list-items__text" id="whyUs">
-							WHY US?
-						</a>
-					</li>
-					<li>
-						<a href="/memberships" className="list-items__text" id="memberships">
-							MEMBERSHIPS
-						</a>
-					</li>
-					<li>
-						<a href="#" className="list-items__text" id="classTime">
-							CLASS TIMETABLES
-						</a>
-					</li>
-					<li>
-						<a href="#" className="list-items__text" id="training">
-							TRAINING
-						</a>
-					</li>
-					<li>
-						<a href="#" className="list-items__text" id="contactUs">
-							CONTACT US
-						</a>
-					</li>
-				</ul>
+				<div className="logo-container">
+					<a href="/" className="logo-container__logo" id="logo">
+						<img src={logo} alt="BennyFitness" height="80" width="150" />
+					</a>
+				</div>
+				<div className="space-container">
+					<div className="nav-items-container">
+						<ul className="list-items">
+							<li>
+								<a href="/whyUs" className="list-items__text" id="whyUs">
+									WHY US?
+								</a>
+							</li>
+							<li>
+								<a
+									href="/memberships"
+									className="list-items__text"
+									id="memberships">
+									MEMBERSHIPS
+								</a>
+							</li>
+							<li>
+								<a href="#" className="list-items__text" id="classTime">
+									CLASS TIMETABLES
+								</a>
+							</li>
+							<li>
+								<a href="#" className="list-items__text" id="training">
+									TRAINING
+								</a>
+							</li>
+							<li>
+								<a href="#" className="list-items__text" id="contactUs">
+									CONTACT US
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
 			</div>
 		</nav>
 	);
