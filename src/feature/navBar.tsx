80e542248cc94e691a43bbbd9e4d1a073e47602f
Ben Kang <rkdgusdn9@gmail.com>
2020-04-10 20:04:23 +1200

    Hamburger menu added on smaller window size

--- src/feature/navBar.tsx
+++ src/feature/navBar.tsx
@@ -1,49 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../images/benny.png';
+import hamburger from '../images/Hamburger.png';
 
 export const NavBar: React.FC = () => {
+	const [setActive, setActiveState] = useState('');
+	const className = 'className';
+
+	function toggleHamburger() {
+		setActiveState(setActive === '' ? 'active' : '');
+	}
+
 	return (
 		<nav className="nav-bar">
-			<div className="nav-bar__container">
-				<div className="logo-container">
-					<a href="/" className="logo-container__logo" id="logo">
-						<img src={logo} alt="BennyFitness" height="80" width="150" />
+			<div className="logo-container">
+				<a href="/" id="logo">
+					<img src={logo} alt="BennyFitness" />
+				</a>
+
+				<div className="hamburger-container">
+					<a>
+						<img
+							className={`hamburger-menu ${setActive}`}
+							src={hamburger}
+							onClick={toggleHamburger}
+						/>
 					</a>
 				</div>
-				<div className="space-container">
-					<div className="nav-items-container">
-						<ul className="list-items">
-							<li>
-								<a href="/whyUs" className="list-items__text" id="whyUs">
-									WHY US?
-								</a>
-							</li>
-							<li>
-								<a
-									href="/memberships"
-									className="list-items__text"
-									id="memberships">
-									MEMBERSHIPS
-								</a>
-							</li>
-							<li>
-								<a href="/classes" className="list-items__text" id="classes">
-									CLASSES
-								</a>
-							</li>
-							<li>
-								<a href="#" className="list-items__text" id="training">
-									TRAINING
-								</a>
-							</li>
-							<li>
-								<a href="#" className="list-items__text" id="contactUs">
-									CONTACT US
-								</a>
-							</li>
-						</ul>
-					</div>
-				</div>
+			</div>
+
+			<div id="navibar" className={`nav-items-container ${setActive}`}>
+				<ul className="list-items">
+					<li>
+						<a href="/whyUs" className="list-items__text" id="whyUs">
+							WHY US?
+						</a>
+					</li>
+					<li>
+						<a href="/memberships" className="list-items__text" id="memberships">
+							MEMBERSHIPS
+						</a>
+					</li>
+					<li>
+						<a href="/classes" className="list-items__text" id="classes">
+							CLASSES
+						</a>
+					</li>
+					<li>
+						<a href="#" className="list-items__text" id="training">
+							TRAINING
+						</a>
+					</li>
+					<li>
+						<a href="#" className="list-items__text" id="contactUs">
+							CONTACT US
+						</a>
+					</li>
+				</ul>
 			</div>
 		</nav>
 	);
