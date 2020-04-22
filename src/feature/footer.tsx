18ffc1ec135d651abeb80290d2185642ddfbbfb3
Ben Kang <rkdgusdn9@gmail.com>
2020-04-23 06:03:16 +1200

    Import Terms, Policy pdf

--- src/feature/footer.tsx
+++ src/feature/footer.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 import Logo1 from '../images/Facebook.png';
 import Logo2 from '../images/LinkedIn.png';
 import Logo3 from '../images/Instagram.png';
+import terms from '../assets/files/terms.pdf';
+import policy from '../assets/files/policy.pdf';
 
 export const Footer: React.FC = () => {
 	return (
 		<footer className="footer margin-x magin-y">
 			<div className="footer__copy">COPYRIGHT © BENNYFITNESS</div>
 			<div className="footer__terms">
-				<a className="footer__terms-item" href="/">
+				<a className="footer__terms-item" href={terms}>
 					TERMS &amp; CONDITIONS{' '}
 				</a>
 				<span className="footer__divider">/</span>
-				<a className="footer__terms-item" href="/">
+				<a className="footer__terms-item" href={policy}>
 					{' '}
 					PRIVACY POLICY
 				</a>
