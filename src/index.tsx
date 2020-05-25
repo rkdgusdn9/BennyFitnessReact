0a95573fe2de22336b77cfba41bf0f939e2599f9
Ben Kang <rkdgusdn9@gmail.com>
2020-05-25 12:47:58 +1200

    Roll back

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import rootReducer from './store/rootReducer';
 
-const store = createStore(rootReducer);
-
-ReactDOM.render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
-	document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
--- src/pages/LoginPage/login.tsx
+++
@@ -1,5 +0,0 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-
-function LoginPage() {}
